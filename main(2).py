092717bcc60ca9930708197a8f091600d6f43ec1
Charles-Meldhine Madi Mnemoi <63333367+cmmm976@users.noreply.github.com>
2020-05-07 15:38:51 +0200

    Upload de la partie à chercher à la maison du TP.

---
+++ main(2).py
@@ -0,0 +1,19 @@
+﻿import triCompte
+import random
+
+#3)
+t1 = []
+for i in range(0,10000+1):
+    t1.append(random.randint(0,100))
+     
+t2 = t1
+     
+print(tri_compte1(t1,max(t1))) #on pourrait donner 100 en argument mais on va se permettre une optimisation ici
+
+#4)
+tri_compte2(t1, max(t1))
+print(t1)
+
+#5)
+tri_compte3(t2)
+print(t2)
---
+++ triCompte(1).py
@@ -0,0 +1,75 @@
+﻿#Exercice 4
+#1)
+
+import random
+
+def tri_compte1(tab,M):
+    r_list = []
+    compte = []
+    for i in range(0,M+1):
+        compte.append(0)
+    
+    for i,val in enumerate(tab):
+            compte[val] = compte[val]+1
+            
+    for i, val in enumerate(compte):
+        for j in range(0,val):
+            r_list.append(i)
+    
+    return r_list
+        
+               
+"""
+2)
+def tri_compte1(tab,M):
+    r_list = []
+    compte = []
+    for i in range(0,M+1):#M+1
+        compte.append(0)#1
+    
+    for i,val in enumerate(tab):#n
+            compte[val] = compte[val]+1#1
+            
+    for i, val in enumerate(compte):#M+1
+        for j in range(0,val):#O(M)
+            r_list.append(i)#1
+    
+    return r_list
+
+(M+1)*1+n*1+(M+1)O(M)*1 = M+1+n+M*O(M) = M(1+O(M))+n = O(n)
+"""
+
+#4)
+def tri_compte2(tab,M):
+    compte = []
+    for i in range(0,M+1):
+        compte.append(0)
+    
+    for i,val in enumerate(tab):
+            compte[val] = compte[val]+1
+            
+    tab.clear()        
+    for i, val in enumerate(compte):
+        for j in range(0,val):
+            tab.append(i)
+            
+#5)
+def max(tab):
+    max = 0
+    for val in tab:
+        if val > max:
+            max = val
+    return max
+
+def tri_compte3(tab):
+    compte = []
+    for i in range(0,max(tab)+1):
+        compte.append(0)
+    
+    for i,val in enumerate(tab):
+            compte[val] = compte[val]+1
+            
+    tab.clear()        
+    for i, val in enumerate(compte):
+        for j in range(0,val):
+            tab.append(i)
